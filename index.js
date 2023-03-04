0311c189389f7439235a71eafcabb1be576a0199
Sahan Kodituwakku <88420631+kodiidok@users.noreply.github.com>
2023-03-04 09:44:20 +0530

    updated index.js

--- index.js
+++ index.js
@@ -25,8 +25,40 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+// An empty date parameter should return the current time in a JSON object with a unix key
+// An empty date parameter should return the current time in a JSON object with a utc key
+// A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number)
+// A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+// A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+// project can handle dates that can be successfully parsed by new Date(date_string)
+// If the input date string is invalid, the API returns an object having the structure { error : "Invalid Date" }
+
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
+});
+
+app.get("/api/:timestamp", (req, res) => {
+  const timestamp = req.params.timestamp;
+  let date = new Date(timestamp);
+  
+  if(!isNaN(Number(timestamp)) && timestamp.length === 13){
+    date = new Date(Number(timestamp));
+  }
+  
+  if (date.toUTCString() === 'Invalid Date') {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });  
+  }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port || 3000);
 });
